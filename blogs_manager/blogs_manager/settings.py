00694f2b94b0757055f497e5545a306e91556547
AdamMisiak <adammisiak3@gmail.com>
2020-10-13 19:23:44 +0200

    Added styles file

--- blogs_manager/blogs_manager/settings.py
+++ blogs_manager/blogs_manager/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'blogs_manager/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blogs_manager/static')
+]
