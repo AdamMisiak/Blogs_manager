cf55b89c3d0bf5e57819cf0321f4fcb34a439667
AdamMisiak <adammisiak3@gmail.com>
2020-10-13 17:35:17 +0200

    Fixed function for pamietnik gieldowy blog

--- blogs_manager/blogs/functions.py
+++ blogs_manager/blogs/functions.py
@@ -12,7 +12,6 @@ def get_info_from_trading_for_a_living():
     blog_post_date = str(soup.find("p", class_="c-post-card__special-date c-post-card__special-date--featured").text)
     blog_post_date = blog_post_date[1:].replace(' ', '.')
     blog_post_date = datetime.datetime.strptime(blog_post_date, '%d.%m.%y').date()
-    
 
     blog_post_list.append(blog_post_date)
     blog_post_list.append(blog_post_title)
@@ -28,7 +27,8 @@ def get_info_from_pamietnik_gieldowy():
     blog_posts = soup.find("div", class_="date-outer")
     blog_post_title = blog_posts.find('div', class_='post-body entry-content').findChildren('span')[0].text
     blog_post_link = soup.find("h3", class_="post-title entry-title").find_next('a')['href']
-    blog_post_date = datetime.datetime.strptime(str(blog_posts.find('div', class_='post-body entry-content').findChildren('b')[1].text), '%d.%m.%Y').date()
+    # print(str(blog_posts.find('div', class_='post-body entry-content').findChildren('b')[0].text).strip())
+    blog_post_date = datetime.datetime.strptime(str(blog_posts.find('div', class_='post-body entry-content').findChildren('b')[0].text).strip(), '%d.%m.%Y').date()
     
     blog_post_list.append(blog_post_date)
     blog_post_list.append(blog_post_title)
@@ -36,5 +36,5 @@ def get_info_from_pamietnik_gieldowy():
 
     return blog_post_list
 
-result = get_info_from_trading_for_a_living()
+result = get_info_from_pamietnik_gieldowy()
 print(result)
