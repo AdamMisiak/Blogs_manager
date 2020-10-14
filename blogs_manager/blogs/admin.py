4290236cd4dd5219d280c5d7fdb24e5e1b9094fb
AdamMisiak <adammisiak3@gmail.com>
2020-10-14 19:09:47 +0200

    Added photo to background

--- blogs_manager/blogs/admin.py
+++ blogs_manager/blogs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'url', 'author', 'genre', 'language')
+  list_display_links = ('id', 'name')
+  list_filter = ('genre', 'language')
+  search_fields = ('name', 'author')
+  list_per_page = 25
+
+admin.site.register(Blog, BlogAdmin)
--- blogs_manager/blogs/models.py
+++ blogs_manager/blogs/models.py
@@ -16,8 +16,9 @@ class Blog(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    genre = models.CharField(choices=GENRE_CHOICES, default="Financial", max_length=30)
+    genre = models.CharField(choices=GENRE_CHOICES, default="IT", max_length=30)
     genre2 = models.CharField(choices=GENRE_CHOICES, blank=True, max_length=30)
+    language = models.CharField(default='Polish', max_length=100)
 
     def __str__(self):
         return self.name
