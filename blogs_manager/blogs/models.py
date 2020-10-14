20ca68cb407079ba2284a41d39e715d122c25e11
AdamMisiak <adammisiak3@gmail.com>
2020-10-14 17:29:39 +0200

    Added Blog model

--- blogs_manager/blogs/models.py
+++ blogs_manager/blogs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+GENRE_CHOICES = (
+    ("Science", "Science"),
+    ("Cooking", "Cooking"),
+    ("Personal", "Personal"),
+    ("Motivational", "Motivational"),
+    ("Travel", "Travel"),
+    ("Lifestyle", "Lifestyle"),
+    ("Humor", "Humor"),
+    ("IT", "IT"),
+    ("Financials", "Financials"),
+)
+
+class Blog(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(choices=GENRE_CHOICES, default="Financial", max_length=30)
+    genre2 = models.CharField(choices=GENRE_CHOICES, blank=True, max_length=30)
+
+    def __str__(self):
+        return self.name
