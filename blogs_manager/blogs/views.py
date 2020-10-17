d6f8beb71fc4acc2e18953a17fa58d697c506c1e
AdamMisiak <adammisiak3@gmail.com>
2020-10-17 11:16:45 +0200

    Added Blogs view and Blogs template

--- blogs_manager/blogs/views.py
+++ blogs_manager/blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def index(request):
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blogs/blogs.html', context)
