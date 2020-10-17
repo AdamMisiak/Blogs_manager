ec6f74f2c01973fb3599ef1b8c6bccf1e1768d32
AdamMisiak <adammisiak3@gmail.com>
2020-10-17 10:41:14 +0200

    Fixed function to scrap blog page

--- blogs_manager/blogs/functions.py
+++ blogs_manager/blogs/functions.py
@@ -31,7 +31,6 @@ def month_string_to_date(month_string):
     return month_date
 
 
-print(month_string_to_date("Październik"))
 def get_info_from_trading_for_a_living():
     page = requests.get("https://www.tradingforaliving.pl/")
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -57,11 +56,15 @@ def get_info_from_pamietnik_gieldowy():
     blog_posts = soup.find("div", class_="date-outer")
     blog_post_title = blog_posts.find('div', class_='post-body entry-content').findChildren('span')[0].text
     blog_post_link = soup.find("h3", class_="post-title entry-title").find_next('a')['href']
-    # blog_post_date = datetime.datetime.strptime(str(blog_posts.find('div', class_='post-body entry-content').findChildren('b')[0].text).strip(), '%d.%m.%Y').date()
-    print(str(blog_posts.find('h2', class_='date-header').findChildren('span')[0].text))
 
     blog_post_year = str(blog_posts.find('h2', class_='date-header').findChildren('span')[0].text)[-4:]
-    print(blog_post_year)
+    blog_post_comma_index = str(blog_posts.find('h2', class_='date-header').findChildren('span')[0].text).find(',')
+    blog_post_month = str(month_string_to_date(str(blog_posts.find('h2', class_='date-header').findChildren('span')[0].text)[blog_post_comma_index+5:14]))
+    blog_post_day = str(blog_posts.find('h2', class_='date-header').findChildren('span')[0].text)[blog_post_comma_index+2:10]
+    
+    blog_post_date_string = blog_post_day+'.'+blog_post_month+'.'+blog_post_year
+    blog_post_date = datetime.datetime.strptime(blog_post_date_string, '%d.%m.%Y').date()
+
     blog_post_list.append(blog_post_date)
     blog_post_list.append(blog_post_title)
     blog_post_list.append(blog_post_link)
