fd303919b74d9837d40181206202f6c04bc121aa
AdamMisiak <adammisiak3@gmail.com>
2020-10-24 19:07:56 +0200

    Added login template and url

--- blogs_manager/users/views.py
+++ blogs_manager/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import auth
 from django.contrib.auth.models import User
 
 
@@ -26,3 +27,17 @@ def register(request):
             print('hasla zle   ')
             return redirect('register')
     return render(request, 'users/register.html')
+
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+    return render(request, 'users/login.html')
+
+def logout(request):
+    auth.logout(request)
+    # messages.success(request, 'You are now logged out')
+    return redirect('index')
