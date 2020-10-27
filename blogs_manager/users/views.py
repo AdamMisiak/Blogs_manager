d66fc6ba5edf4959ed1e549254ea78f7a72db3cf
AdamMisiak <adammisiak3@gmail.com>
2020-10-27 16:31:29 +0100

    Added function for another blog in account view

--- blogs_manager/users/views.py
+++ blogs_manager/users/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth.models import User
+from blogs.models import Blog, BlogPost
+from blogs.functions import *
+
+from .functions import create_new_blog_post
 
 
 def account(request):
+    blog_posts = BlogPost.objects.all()
+    create_new_blog_post(get_info_from_trading_for_a_living, 'Trading for a living')
+    create_new_blog_post(get_info_from_pamietnik_gieldowy, 'Pamiętnik Giełdowy')
+    # trading_for_a_living = get_info_from_trading_for_a_living()
+    # blog_post, created = BlogPost.objects.get_or_create(
+    #     name=trading_for_a_living[0],
+    #     url=trading_for_a_living[1],
+    #     added=trading_for_a_living[2],
+    #     blog=Blog.objects.get(name="Trading for a living"),
+    # )
+    # blog_post.save()
 
-    return render(request, 'users/account.html')
+    context = {"blog_posts": blog_posts}
+
+    return render(request, 'users/account.html', context)
 
 def register(request):
     if request.method == "POST":
