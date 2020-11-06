fb0b7826e5e2e2db5f1f28cb94841f72473cea7a
AdamMisiak <adammisiak3@gmail.com>
2020-11-06 17:26:26 +0100

    Added new blog to base and created celery task

--- blogs_manager/users/functions.py
+++ blogs_manager/users/functions.py
@@ -15,7 +15,7 @@ def create_new_blog_post(get_info_function, blog_name):
     )
     blog_post.save()
 
-@periodic_task(run_every=(crontab(hour='*/1')), name="check_new_blog_posts", ignore_result=True)
+@periodic_task(run_every=(crontab(minute='*/1')), name="check_new_blog_posts", ignore_result=True)
 def check_new_blog_posts():
     create_new_blog_post(get_info_from_trading_for_a_living, 'Trading for a living')
     create_new_blog_post(get_info_from_pamietnik_gieldowy, 'Pamiętnik Giełdowy')
@@ -23,3 +23,4 @@ def check_new_blog_posts():
     create_new_blog_post(get_info_from_independent_trader, 'Independent Trader')
     create_new_blog_post(get_info_from_usstocks, 'USStocks')
     create_new_blog_post(get_info_from_system_trader, 'System Trader')
+    create_new_blog_post(get_info_from_spekulant, 'Spekulant')
