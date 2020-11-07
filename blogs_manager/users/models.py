4becc83f3d2fe77472cfb3a1005f99878d43ad0c
AdamMisiak <adammisiak3@gmail.com>
2020-11-07 10:46:32 +0100

    Added blog subscriber model

--- blogs_manager/users/models.py
+++ blogs_manager/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from blogs.models import Blog
+
+from datetime import datetime
+
 
 # Create your models here.
+class BlogSubscriber(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='subscribed_blog')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_subscriber')
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    email_notification = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} subscribed by {} in {}'.format(self.blog, self.user, self.date)
