c7923fb9b7b7257298c9d5db331275e213fcc876
AdamMisiak <adammisiak3@gmail.com>
2020-11-07 10:54:23 +0100

    Added admin tool to new model

--- blogs_manager/users/admin.py
+++ blogs_manager/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import BlogSubscriber
 
-# Register your models here.
+
+class BlogSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("id", "blog", "user", "date", "email_notification")
+    list_display_links = ("id", "blog")
+    list_filter = ("blog", "user")
+    search_fields = ("blog", "user")
+    list_per_page = 25
+
+
+
+admin.site.register(BlogSubscriber, BlogSubscriberAdmin)
+
