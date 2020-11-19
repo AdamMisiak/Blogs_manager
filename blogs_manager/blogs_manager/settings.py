60492d78ae0ec7e676687c5fd9bc25c1ee5137b0
AdamMisiak <adammisiak3@gmail.com>
2020-11-19 16:37:57 +0100

    Changed db to Postgres

--- blogs_manager/blogs_manager/settings.py
+++ blogs_manager/blogs_manager/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "blogs_manager.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogs_manager',
+        'USER': 'adam',
+        'PASSWORD': 'adam',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
