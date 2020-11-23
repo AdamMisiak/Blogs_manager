a9c4aacaf884c74dc5a21e287385c3874595dbab
AdamMisiak <adammisiak3@gmail.com>
2020-11-23 16:30:33 +0100

    Added tests for blogs app

--- blogs_manager/blogs/test_blogs.py
+++ blogs_manager/blogs/test_blogs.py
@@ -1,5 +1,7 @@
 import pytest
 
+from .models import Blog
+
 from django.test import Client
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
@@ -10,10 +12,30 @@ def login():
     logged_in = client.login(username="admin", password="admin")
     return logged_in
 
+@pytest.fixture
+def create_blog():
+    blog = Blog.objects.create(name="Name", url="url", author="Author", genre="Genre", language="Language")
+    return blog
+
 class TestBlogsViews:
     @pytest.mark.django_db
     def test_blogs_view_status_code(self, login):
         client = Client()
         url = reverse("blogs")
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+    @pytest.mark.django_db
+    def test_blog_info_view_status_code(self, login, create_blog):
+        client = Client()
+        url = reverse("blog_info", kwargs={'pk':1})
+        response = client.get(url)
+        assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_subscribed_view_status_code(self, login, create_blog):
+        client = Client()
+        url = reverse("subscribed")
         response = client.get(url)
         assert response.status_code == 200
