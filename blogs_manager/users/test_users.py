fe7ba6dcb0cd34e67924e21c6dcce7ced929d709
AdamMisiak <adammisiak3@gmail.com>
2020-11-24 16:50:49 +0100

    Added register user test

--- blogs_manager/users/test_users.py
+++ blogs_manager/users/test_users.py
@@ -1,6 +1,6 @@
 import pytest
 
-from django.test import Client
+from django.test import Client, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User
 
@@ -21,4 +21,15 @@ class TestUsersViews:
         url = reverse("account")
         response = client.get(url)
         assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_register_view_status_code(self):
+        client = Client()
+        login = client.login(username="admin", password="admin")
+        url = reverse("register")
+        response = client.post(url, {'first_name':'first_name','last_name':'last_name','username':'username','email':'email@email.com','password':'password', 'password2':'password'})
+        assert response.status_code == 302
+        assert response.url == "/"
+
+
 
