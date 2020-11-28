387f4168442c4a737f21575f9b9e2f441e9b7664
AdamMisiak <adammisiak3@gmail.com>
2020-11-28 17:00:29 +0100

    Fixed subscription information

--- blogs_manager/pages/views.py
+++ blogs_manager/pages/views.py
@@ -5,16 +5,14 @@ from users.models import BlogSubscriber
 
 def index(request):
     blog_posts = BlogPost.objects.all().order_by('-added', 'name')
-    # user = request.user
-    # blogs_subscribed = BlogSubscriber.objects.filter(user=user)
-    # print(blogs_subscribed)
-    # for i in blog_posts:
-    #     print(i.blog.subscribed_blog.all().first())
-
-
+    if request.user.is_authenticated:
+        blogs_subscribed = BlogSubscriber.objects.filter(user=request.user)
+    else:
+        blogs_subscribed = []
 
     context = {
-        "blog_posts": blog_posts
+        "blog_posts": blog_posts,
+        "blogs_subscribed": blogs_subscribed
     }
 
     return render(request, "pages/index.html", context)
