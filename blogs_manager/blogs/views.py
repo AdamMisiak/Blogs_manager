2b75e2a1e88a0542e0809afbecdad06145cd3669
AdamMisiak <adammisiak3@gmail.com>
2020-12-02 16:39:39 +0100

    Added average number of posts per month

--- blogs_manager/blogs/views.py
+++ blogs_manager/blogs/views.py
@@ -6,6 +6,16 @@ from users.models import BlogSubscriber
 from .models import Blog, BlogPost
 from .functions import get_info_from_trading_for_a_living
 
+def avg_number_of_posts_per_month(blog):
+    posts_in_month = {}
+    blog_posts = BlogPost.objects.filter(blog=blog).order_by('-added', 'name')
+    for post in blog_posts:
+        if post.added.month in posts_in_month.keys():
+            posts_in_month[post.added.month] += 1
+        else:
+            posts_in_month[post.added.month] = 1
+    average = round(blog_posts.count()/len(posts_in_month), 2)
+    return average
 
 def index(request):
     blogs = Blog.objects.all()
@@ -51,6 +61,7 @@ def blog_info(request, pk):
     blog = Blog.objects.get(id=pk)
     blog_posts = BlogPost.objects.filter(blog=blog).order_by('-added', 'name')
     blog_posts_count = BlogPost.objects.filter(blog=blog).count()
+    blog_posts_per_month = avg_number_of_posts_per_month(blog)
 
     if request.user.is_authenticated:
         subscribed_blogs = [blog_subscriber.blog.id for blog_subscriber in BlogSubscriber.objects.filter(user=request.user)]
@@ -61,8 +72,8 @@ def blog_info(request, pk):
                "blog": blog,
                "blog_posts": blog_posts,
                "blog_posts_count": blog_posts_count,
-               "subscribed_blogs": subscribed_blogs
+               "subscribed_blogs": subscribed_blogs,
+               "blog_posts_per_month": blog_posts_per_month,
                }
 
     return render(request, "blogs/blog_info.html", context)
-
