cb198a4de3a6c7969ba21b19d115be929f90af65
AdamMisiak <adammisiak3@gmail.com>
2020-12-07 16:20:51 +0100

    Added subscribed info styles

--- blogs_manager/users/functions.py
+++ blogs_manager/users/functions.py
@@ -17,7 +17,7 @@ def create_new_blog_post(get_info_function, blog_name):
 
 @periodic_task(run_every=(crontab(minute='*/1')), name="check_new_blog_posts", ignore_result=True)
 def check_new_blog_posts():
-    create_new_blog_post(get_info_from_trading_for_a_living, 'Trading for a living')
+    # create_new_blog_post(get_info_from_trading_for_a_living, 'Trading for a living')
     create_new_blog_post(get_info_from_pamietnik_gieldowy, 'Pamiętnik Giełdowy')
     create_new_blog_post(get_info_from_inwestomat, 'Inwestomat.eu')
     create_new_blog_post(get_info_from_independent_trader, 'Independent Trader')
