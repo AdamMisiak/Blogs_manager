1fc2e89ba2fc71e4c9f0a10cbbbd53dfa8a07f89
AdamMisiak <adammisiak3@gmail.com>
2021-01-07 16:13:43 +0100

    Added signal for logging in

--- blogs_manager/users/models.py
+++ blogs_manager/users/models.py
@@ -1,8 +1,11 @@
-from django.contrib.auth.models import User
-from django.db import models
-from blogs.models import Blog, BlogPost
-
 from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in
+from django.db import models
+from django.dispatch import receiver
+
+from blogs.models import Blog, BlogPost
 
 class BlogSubscriber(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='subscribed_blog')
@@ -31,12 +34,14 @@ ACTIONS = (
 
 class UserLog(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     object_id = models.TextField()
     action = models.CharField(max_length=1, choices=ACTIONS)
 
     @receiver(user_logged_in)
     def user_logged_in(sender, request, user, **kwargs):
-        UserLog.create_log(sender, 'login', 'L', user)
+        UserLog.objects.create(user=request.user,
+                        object_id='login'.__str__(),
+                        action='L')
 
 
