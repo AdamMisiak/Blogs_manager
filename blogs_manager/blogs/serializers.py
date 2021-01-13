a5d548f4255f0a93ee9998aa4010865a93f76436
AdamMisiak <adammisiak3@gmail.com>
2021-01-13 17:35:18 +0100

    Added blog details serializer and custom field

--- blogs_manager/blogs/serializers.py
+++ blogs_manager/blogs/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from .models import Blog
+from .models import Blog, BlogPost
 
-class BlogSerializer(serializers.ModelSerializer):
+class BlogListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Blog
         fields = '__all__'
+
+class BlogDetailsSerializer(serializers.ModelSerializer):
+    number_of_blog_posts = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Blog
+        fields = ['name', 'url', 'author', 'genre', 'language', 'number_of_blog_posts'] 
+
+    def get_number_of_blog_posts(self, obj):
+        return BlogPost.objects.filter(blog=obj).count()
+
