613aefd476a4f2434cc87b044757d25b45c717a8
AdamMisiak <adammisiak3@gmail.com>
2021-01-21 16:23:28 +0100

    Added 2 page components

---
+++ blogs_manager/frontend/src/components/IndexPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+export default class IndexPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>
+            INDEX PAGE
+        </p>
+    }
+}
