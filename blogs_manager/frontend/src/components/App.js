96652ce78c593339c0e5eca1eb5988973d919b84
AdamMisiak <adammisiak3@gmail.com>
2021-01-21 16:23:28 +0100

    Added 2 page components

--- blogs_manager/frontend/src/components/App.js
+++ blogs_manager/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom"
+import IndexPage from './IndexPage'
 
 export default class App extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ export default class App extends Component {
 
     render() {
         return <h1>
-            TEST REACT
+            <Index />
         </h1>
     }
 }
