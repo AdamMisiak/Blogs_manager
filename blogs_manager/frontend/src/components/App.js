c7cee215f8df33f6f2cd49caec19b8a60a940eeb
AdamMisiak <adammisiak3@gmail.com>
2021-01-28 18:04:40 +0100

    Added redux initial

--- blogs_manager/frontend/src/components/App.js
+++ blogs_manager/frontend/src/components/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { render } from "react-dom"
+
 import IndexPage from "./IndexPage";
 import Header from "./layout/Header"
+
+import { Provider } from 'react-redux';
+import store from '../store'
 
 export default class App extends Component {
     constructor(props) {
@@ -10,9 +14,12 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
-                <Header />
-            </div>
+            <Provider store={store}>
+                <div>
+                    <Header />
+                </div>
+            </Provider>
+
         )
     }
 }
