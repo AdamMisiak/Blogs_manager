47a032cc6678821859eecfdb08fe9980b1da38ec
AdamMisiak <adammisiak3@gmail.com>
2021-02-23 21:25:00 +0100

    Changed structure of app

--- frontend/src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const GET_BLOGS = 'GET_BLOGS';
-export const WRITE_TASKS = 'WRITE_TASKS';
-export const CHANGE_PRIORITY = 'CHANGE_PRIORITY';
-export const CHANGE_GROUP = 'CHANGE_GROUP';
-export const COMPLETE_TASK = 'COMPLETE_TASK';
-export const HIDE_TASK = 'HIDE_TASK';
