e7356296661befb95dd8351eb80b35c1eefc4605
AdamMisiak <adammisiak3@gmail.com>
2021-02-25 16:19:29 +0100

    Added showcase component

---
+++ frontend/src/components/layout/Showcase.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../../styles/Showcase.css';
+
+const Showcase = ({
+
+}) => {
+
+  return(
+    <section id="showcase-inner" className="py-5 text-black">
+        <div className="container">
+            <div className="row text-center">
+                <div className="col-md-12">
+                    <h1 className="display-4">Managing <span style={{color:"#e7e7e7"}}>Blogs</span> Has Never Been So Easy</h1>
+                    <p className="lead">Subscribe, manage and follow your favourite blogs in one place!  </p>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default Showcase;
