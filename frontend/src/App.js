0a96b9fe45d6a3ad4115242af47bbf1842903c0d
AdamMisiak <adammisiak3@gmail.com>
2021-03-02 17:11:57 +0100

    Added alerts react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { Provider } from 'react-redux';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic'l
 import store from './store'
 
 import './styles/App.css';
@@ -7,16 +9,22 @@ import Header from './components/layout/Header'
 import Breadcrumb from './components/layout/Breadcrumb';
 import IndexPage from './components/screens/IndexPage';
 
+
+const alertOptions = {
+  timeout: 3000,
+  position: 'bottom right'
+}
+
 function App() {
   return (
     <Provider store={store}>
-
-      <Header />
-      <Breadcrumb />
-      <IndexPage />
-
+      <AlertProvider>
+        <Header />
+        <Breadcrumb />
+        <IndexPage />
+      </AlertProvider>
     </Provider>
-    
+
   );
 }
 
