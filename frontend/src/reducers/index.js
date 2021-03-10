6e92345b31a1d88787641ab348998ca215f54b10
Adam  Misiak <adammisiak@MacBook-Pro-Adam.local>
2021-03-10 20:22:28 +0100

    Added current location in blog details

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import blogDetailsReducer from './BlogDetails';
 import blogPostsReducer from './BlogPosts'
 import blogsReducer from './Blogs'
 
 export default combineReducers({
     blogPosts: blogPostsReducer,
     blogs: blogsReducer,
+    blogDetails: blogDetailsReducer,
 
 });
