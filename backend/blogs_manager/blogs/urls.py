acc57bf01281f49132db60582411180c3ad7f0b5
Adam Misiak <adammisiak3@gmail.com>
2021-03-17 14:37:56 +0100

    Added django filters to endpoint

--- backend/blogs_manager/blogs/urls.py
+++ backend/blogs_manager/blogs/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="blogs"),
-    path("<int:pk>/", views.blog_info, name="blog_info"),
-    path("subscribed/", views.subscribed, name="subscribed"),
+    # path("", views.index, name="blogs"),
+    # path("<int:pk>/", views.blog_info, name="blog_info"),
+    # path("subscribed/", views.subscribed, name="subscribed"),
 ]
