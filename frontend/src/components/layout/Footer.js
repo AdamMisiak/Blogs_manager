0f5a69b5e361a4834cbc534c14279dd4cff96432
Adam Misiak <adammisiak3@gmail.com>
2021-03-22 07:44:24 +0100

    Added footera component

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import '../../styles/Showcase.css';
+
+const Footer = () => {
+
+  return(
+    <footer id="main-footer" className="py-4 bg-primary text-white text-center">
+        Copyright &copy;
+        <span className="year">2021</span> Blogs Manager
+    </footer>
+  );
+}
+
+export default Footer;
