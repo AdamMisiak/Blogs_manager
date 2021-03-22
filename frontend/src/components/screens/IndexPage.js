c6f72502724ebf2a188167c2fc2b79b42ccc6a93
Adam Misiak <adammisiak3@gmail.com>
2021-03-22 09:19:32 +0100

    Refactored indexPage component

--- frontend/src/components/screens/IndexPage.js
+++ frontend/src/components/screens/IndexPage.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import ClipLoader from "react-spinners/ClipLoader";
+// material ui
+
 
 import Pagination from '@material-ui/lab/Pagination';
 
@@ -10,47 +12,67 @@ import Breadcrumb from '../layout/Breadcrumb';
 import Showcase from '../layout/Showcase';
 import Alerts from '../layout/Alerts';
 
+import "../../styles/Pagination.css";
+
 const override = "display: block; margin: 0 auto;";
 
 function IndexPage({
     blogPosts,
     getBlogPosts
 }) {
-    var page = 1;
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        getBlogPosts(page)
-    }, [])
+        getBlogPosts({
+            page: page,
+        })
+    }, [page])
 
-    return blogPosts.loading ? (
-        <div>
-            <ClipLoader color="rgba(55, 113, 189, 0.9)" loading={true} css={override} size={100} />
-        </div>
-    ) : blogPosts.error ? (
-        <h2>
-            <Alerts 
-              type="error"
-              message={blogPosts.error}
+    const handlePageChange = (event, value) => {
+        setPage(value);
+      }
+
+    return (
+    <div>
+        {/* style do zmiany bo nazwa uzywana juz */}
+        <div className='breadcrumb-wrapper'>
+            <Breadcrumb 
+                current='Latest'
             />
-        </h2>
-    ) : (
-                <div>
-                    <Breadcrumb 
-                        current='Latest'
+        </div>
+        <div className='showcase'>
+            <Showcase />
+        </div>
+        {blogPosts.loading ? (
+            <div className='loader'>
+                <ClipLoader color="rgba(55, 113, 189, 0.9)" loading={true} css={override} size={100} />
+            </div> ) : ( null )}
+        {blogPosts.error ? (
+            <div className='alerts'>
+                <Alerts 
+                    type="error"
+                    message={blogPosts.error}
+                />
+            </div> ) : ( null )}
+        {!blogPosts.loading && !blogPosts.error ? (
+            <div className='blogposts'>
+                {blogPosts.data.map(blogPost => (
+                    <BlogPost
+                        key={blogPost.id}
+                        blogPost={blogPost}
                     />
-                    <Showcase />
-                    {blogPosts.data.map(blogPost => (
-                        <BlogPost
-                            key={blogPost.id}
-                            blogPost={blogPost}
-                        />
-                    ))}
-                    <Pagination 
-                        count={Math.floor(blogPosts.dataCount/20)} 
-                        variant="outlined" 
-                    />
-                </div>
-            );
+                ))}
+            </div> ) : ( null )}
+        <div className='pagination'>
+            <Pagination 
+                count={Math.floor(blogPosts.dataCount/20)} 
+                variant="outlined" 
+                onChange={handlePageChange}
+                className="pagination"
+            />
+        </div>
+    </div>
+    )
 }
 
 const mapStateToProps = state => ({
