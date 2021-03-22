b4cd67958fc1c8229878d602d42cafc54820cbfc
Adam Misiak <adammisiak3@gmail.com>
2021-03-22 14:41:54 +0100

    Refactored blog details page

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,10 +3,12 @@ import '../../styles/Showcase.css';
 const Footer = () => {
 
   return(
-    <footer id="main-footer" className="py-4 bg-primary text-white text-center">
+    <div className="footer">
+      <footer id="main-footer" className="py-4 bg-primary text-white text-center">
         Copyright &copy;
         <span className="year">2021</span> Blogs Manager
-    </footer>
+      </footer>
+    </div>
   );
 }
 
