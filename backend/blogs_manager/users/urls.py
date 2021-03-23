a36987abb415171187a6f0520d6fe7f04698f68a
Adam Misiak <adammisiak3@gmail.com>
2021-03-23 16:15:56 +0100

    Added login endpoint

--- backend/blogs_manager/users/urls.py
+++ backend/blogs_manager/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from knox import views as knox_views
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
-    path('api/auth/register', RegisterView.as_view())
+    path('api/auth/register', RegisterView.as_view()),
+    path('api/auth/login', LoginView.as_view())
     # path("account/", views.account, name="account"),
     # path("register/", views.register, name="register"),
     # path("login/", views.login, name="login"),
