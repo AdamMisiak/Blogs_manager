53844add1b785c5dc2dff19498cf3d121adea7d6
Adam Misiak <adammisiak3@gmail.com>
2021-03-23 17:00:36 +0100

    Added knox token

--- backend/blogs_manager/users/urls.py
+++ backend/blogs_manager/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from knox import views as knox_views
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserView
 
 urlpatterns = [
-    path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterView.as_view()),
-    path('api/auth/login', LoginView.as_view())
+    path('api/auth/login', LoginView.as_view()),
+    path('api/auth/user', UserView.as_view()),
+    path('api/auth', include('knox.urls')),
     # path("account/", views.account, name="account"),
     # path("register/", views.register, name="register"),
     # path("login/", views.login, name="login"),
