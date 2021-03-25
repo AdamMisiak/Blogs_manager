fba73503c0f39ba124c65dd9901f2f2fbcdce6df
Adam Misiak <adammisiak3@gmail.com>
2021-03-25 13:37:52 +0100

    Added login page component

--- frontend/src/components/screens/LoginPage.js
+++ frontend/src/components/screens/LoginPage.js
@@ -1,61 +1,117 @@
-// import React, { useEffect } from 'react';
-// import { connect } from 'react-redux';
-// import ClipLoader from "react-spinners/ClipLoader";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 
-// import { getBlogs } from '../../actions/Blogs';
-// import Blog from '../common/Blog';
-// import Showcase from '../layout/Showcase';
+import "../../styles/Login.css";
 
-// const override = "display: block; margin: 0 auto;";
 
-// function LoginPage({}) {
-//     useEffect(() => {
-//         getBlogs()
-//     }, [])
+function RegisterPage() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
-//     return (
-//         <div id="register" class="bg-light py-5">
-//         <div class="container">
-//             <div class="row">
-//                 <div class="col-md-6 mx-auto">
-//                     <div class="card-login">
-//                         <div class="card-header bg-primary text-white">
-//                             <h4>
-//                                 <i class="fas fa-sign-in-alt"></i> Login</h4>
-//                         </div>
-//                         <div class="card-body">
-    
-                        
-                            
-//                             <form action="{% url 'login' %}" method="POST">
-//                                 {% csrf_token %}
-//                                 <div class="form-group">
-//                                     <label for="username">Username</label>
-//                                     <input type="text" name="username" class="form-control" required>
-//                                 </div>
-//                                 <div class="form-group">
-//                                     <label for="password2">Password</label>
-//                                     <input type="password" name="password" class="form-control" required>
-//                                 </div>
-//                                 <input type="submit" value="Login" class="btn btn-secondary btn-block">
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//             );
-// }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // const { username, email, password, password2 } = state;
+        // if (password !== password2) {
+        //     console.log('passwords wrong')
+        //   this.props.createMessage({ passwordNotMatch: 'Passwords do not match' });
+        // } else {
+        //   const newUser = {
+        //     username,
+        //     password,
+        //   };
+        //   this.props.register(newUser);
+        // }
+    };
 
-// const mapStateToProps = state => ({
-//     blogs: state.blogs
-// });
+    const onChange = e => console.log(e.target.name);
 
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         getBlogs: () => dispatch(getBlogs())
-//     }
-// }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
+    return (
+        <section id="register" className="bg-light py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <div className="card-register">
+                            <div className="card-header bg-primary text-white">
+                                <h4>
+                                <i className="fas fa-user-plus"></i> Login</h4>
+                            </div>
+                            <div className="card-body">
+                            <form onSubmit={onSubmit}>
+                                <div className="form-group">
+                                  <label>Username</label>
+                                  <input
+                                    type="text"
+                                    className="form-control"
+                                    name="username"
+                                    onChange={onChange}
+                                    value={username}
+                                  />
+                                </div>
+                                <div className="form-group">
+                                  <label>Password</label>
+                                  <input
+                                    type="password"
+                                    className="form-control"
+                                    name="password"
+                                    onChange={onChange}
+                                    value={password}
+                                  />
+                                </div>
+                                <div className="form-group">
+                                  <button type="submit" className="btn btn-primary">
+                                    Login
+                                  </button>
+                                </div>
+                                <p>
+                                  Don't have an account? <Link to="/register">Register</Link>
+                                </p>
+                              </form>
+                                {/* <form action="" method="POST">
+                                    <div className="form-group">
+                                        <label for="first_name">First Name</label>
+                                        <input type="text" name="first_name" className="form-control" required>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="last_name">Last Name</label>
+                                        <input type="text" name="last_name" className="form-control" required>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="username">Username</label>
+                                        <input type="text" name="username" className="form-control" required>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" name="email" className="form-control" required>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="password2">Password</label>
+                                        <input type="password" name="password" className="form-control" required>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="password">Confirm Password</label>
+                                        <input type="password" name="password2" className="form-control" required>
+                                    </div>
+                                    <input type="submit" value="Register" className="btn btn-secondary btn-block">
+                                </form> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+const mapStateToProps = state => ({
+    // blogs: state.blogs
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // getBlogs: ({page}) => dispatch(getBlogs(page))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterPage);
