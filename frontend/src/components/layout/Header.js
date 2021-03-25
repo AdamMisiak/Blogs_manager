21c73ccd7c5efba7f5a618409ba17594855a0a2b
Adam Misiak <adammisiak3@gmail.com>
2021-03-25 16:23:07 +0100

    Changed header display

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPlus, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus, faSignInAlt, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 import "../../styles/Header.css";
 import logo from "../../images/logo.png";
 
 const Header = () => {
+  const auth = useSelector(state => state.auth);
+  console.log(auth)
+
   return (
     <div className='header'>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -28,11 +32,36 @@ const Header = () => {
           </ul>
         </div>
 
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          {/* <ul className="navbar-nav"> */}
+            {auth.isAuthenticated ? (
+              <ul className="navbar-nav">
+                <li className="nav-item justify-content-end">
+                  <Link className="nav-link" to="/account">
+                    <FontAwesomeIcon icon={faUser} /> Account
+                  </Link>
+                </li>
+                <li className="nav-item justify-content-end">
+                  <Link className="nav-link" to="/logout">
+                    <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+                  </Link>
+                </li>
+              </ul>
+            ) : ( 
+              <ul className="navbar-nav">
+                <li className="nav-item justify-content-end">
+                  <Link className="nav-link" to="/register">
+                    <FontAwesomeIcon icon={faUserPlus} /> Register
+                  </Link>
+                </li>
+                <li className="nav-item justify-content-end">
+                  <Link className="nav-link" to="/login">
+                    <FontAwesomeIcon icon={faSignInAlt} /> Login
+                  </Link>
+                </li>
+              </ul>
+              
+             )}
             {/* {% if user.is_authenticated %}
                         <li className="nav-item justify-content-end">
                             <a className="nav-link" href="{% url 'account' %}"><i className="fas fa-user"></i> Account</a>
@@ -41,23 +70,13 @@ const Header = () => {
                             <a className="nav-link" href="{% url 'logout' %}"><i className="fas fa-sign-out-alt"></i> Logout</a>
                         </li>
                         {% else %} */}
-            <li className="nav-item justify-content-end">
-              <Link className="nav-link" to="/register">
-                  <FontAwesomeIcon icon={faUserPlus} /> Register
-              </Link>
-            </li>
-            <li className="nav-item justify-content-end">
-              <Link className="nav-link" to="/login">
-                <FontAwesomeIcon icon={faSignInAlt} /> Login
-              </Link>
-            </li>
+
+            
             {/* {% endif %} */}
-          </ul>
         </div>
       </nav>
     </div>
   );
 };
 
-// export default connect(mapStateToProps, { getBlogPosts })(Header);
 export default Header;
