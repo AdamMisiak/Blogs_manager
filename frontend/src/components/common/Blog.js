9b7b93b3d8ae0c19da5c22240c73d680eb1709f5
Adam Misiak <adammisiak3@gmail.com>
2021-03-31 14:34:50 +0200

    Added subscribing blogs

--- frontend/src/components/common/Blog.js
+++ frontend/src/components/common/Blog.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import Moment from 'moment';
@@ -12,19 +12,25 @@ const Blog = ({
     blog,
 }) => {
     const auth = useSelector(state => state.auth);
-    const [subscribed, setsubscribed] = useState(false);
+    const subscribedBlogs = useSelector(state => state.subscribedBlogs);
     const dispatch = useDispatch();
+
+    const [subscribed, setSubscribed] = useState(false);
+    const user = auth.user.id
+
+    useEffect(() => {
+        subscribedBlogs.data.map(subscribedBlog => {
+            if (blog.id === subscribedBlog.id) {
+                setSubscribed(true)
+            }
+        })
+    }, [])
 
     const onClick = (e) => {
         e.preventDefault();
         dispatch(postSubscribeBlog(auth.user.id, blog.id));
-        setsubscribed(!subscribed)
+        setSubscribed(!subscribed)
     };
-
-    const checkIfBlogSubscribed = (blogId) => {
-        console.log(blogId)
-    }
-    checkIfBlogSubscribed(10)
 
     return (
         <div className="card text-center">
@@ -35,23 +41,17 @@ const Blog = ({
             </div>
             <div className="card-body">
                 <h5 className="card-title">{blog.author}</h5>
-                {blog.genre} | <a href="{blog.url}" className="card-text">{blog.url}</a>
+                {blog.genre} | <a href={blog.url} className="card-text">{blog.url}</a>
             </div>
 
             <div className="subscribe card-footer text-muted">
                 <div className="card-button">
                     <button
-                        className={subscribed ? 'button subscribe-button' : 'button unsubscribe-button'} 
+                        className={subscribed ? 'button unsubscribe-button' : 'button subscribe-button'} 
                         onClick={onClick}>
-                        {subscribed ? 'Subscribe' : 'Unsubscribe'}
+                        {subscribed ? 'Unsubscribe' : 'Subscribe'}
                     </button>
                 </div>
-    
-                        {/* <div className="card-button">
-                            <button className='button unsubscribe-button' id="blog{{ blog.id }}" data-catid='{{ blog.id }}'>Unsubscribe</button>
-                        </div>
-                    {% endif %} */}
-
                 <hr className="my-2">
                 </hr>
                 Last post published: {Moment(blog.last_post_added).format('DD-MM-YYYY')}
