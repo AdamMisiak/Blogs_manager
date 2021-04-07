d995f72c8950128c28d3c3b959f9136d3b4f23be
Adam Misiak <adammisiak3@gmail.com>
2021-04-07 13:43:04 +0200

    Added filters to blog posts

--- backend/blogs_manager/blogs/filters.py
+++ backend/blogs_manager/blogs/filters.py
@@ -3,10 +3,12 @@ from django_filters import FilterSet, CharFilter, NumberFilter
 from .models import Blog, BlogPost
 
 class BlogPostFilter(FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='icontains')
     blog_id = NumberFilter(field_name='blog_id')
 
     class Meta:
         model = BlogPost
         fields = [
+            'name',
             'blog_id',
         ]
