96c4cd7a8a7ccf2c4c9162f0ae7b6f65ce671c77
Adam Misiak <adammisiak3@gmail.com>
2021-04-09 11:02:01 +0200

    Added sorting buttons

--- frontend/src/components/layout/Filters.js
+++ frontend/src/components/layout/Filters.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getBlogPosts } from '../../actions/BlogPosts';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faSort, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons';
 
 import '../../styles/Breadcrumb.css';
 
@@ -16,6 +16,8 @@ const Filters = ({
 }) => {
     const dispatch = useDispatch();
     const [inputSearching, setInputSearching] = useState("")
+
+    const [clicks, setClicks] = useState(0);
     
     const inputSearchingHandler = e => setInputSearching(e.target.value);
 
@@ -32,6 +34,15 @@ const Filters = ({
         }
     }, [inputSearching, page])
 
+    const onClick = (e) => {
+        e.preventDefault();
+        setClicks(clicks+1)
+
+        if (clicks === 2) {
+            setClicks(0)
+        }
+    };
+
     
   return(
     <div className='filters'>
@@ -45,7 +56,28 @@ const Filters = ({
             </div>
         </div>
         <div className="filters-sorting">
-            SORTTTT
+                { clicks === 0 ? (
+                    <button type="button" class="sorting-button" onClick={onClick}>
+                        Sort by date <FontAwesomeIcon icon={faSort} />
+                    </button>
+                ) : ( null )}
+                { clicks === 1 ? (
+                    <button type="button" class="sorting-button" onClick={onClick}>
+                        Sort by date <FontAwesomeIcon icon={faSortUp} />
+                    </button>
+                ) : (null )}
+                { clicks === 2 ? (
+                    <button type="button" class="sorting-button" onClick={onClick}>
+                        Sort by date <FontAwesomeIcon icon={faSortDown} />
+                    </button>
+                ) : (null )}
+                
+            
+            <button type="button" class="sorting-button">Sort by name</button>
+            <button type="button" class="sorting-button">Sort by author</button>
+            <button type="button" class="sorting-button">Sort by blog</button>
+            <button type="button" class="sorting-button">Sort by genre</button>
+
         </div>
     </div>
   );
