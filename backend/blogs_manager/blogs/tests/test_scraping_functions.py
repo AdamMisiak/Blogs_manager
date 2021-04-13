f29f7cae9556438d3cc16bc6cbfe36230f4b92e5
Adam Misiak <adammisiak3@gmail.com>
2021-04-13 13:19:00 +0200

    Added scraping function test

--- backend/blogs_manager/blogs/tests/test_scraping_functions.py
+++ backend/blogs_manager/blogs/tests/test_scraping_functions.py
@@ -1,10 +1,16 @@
 import datetime
 
 from django.test import TestCase
+from blogs.models import BlogPost, Blog
 from blogs.functions import month_string_to_date, get_info_from_inwestomat
+from users.functions import create_new_blog_post
 
 
 class TestScrapingFunctions(TestCase):
+
+    def setUp(self):
+        self.blog_one = Blog.objects.create(name="test_blog_one", url="www.blogone.com", author="test_author_one", genre="IT",
+                                            language="test_language")
               
     def test_month_string_to_date(self):
         assert month_string_to_date('jan') == 1
@@ -24,5 +30,9 @@ class TestScrapingFunctions(TestCase):
         assert type(scraping_results[1]) == str
         assert type(scraping_results[2]) == datetime.date
 
+    def test_create_new_blog_post(self):
+        assert BlogPost.objects.all().count() == 0
 
-    
+        create_new_blog_post(get_info_from_inwestomat, 'test_blog_one')
+
+        assert BlogPost.objects.all().count() == 1
