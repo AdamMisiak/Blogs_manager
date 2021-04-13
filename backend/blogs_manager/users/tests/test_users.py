a9f17a7489fa8e0aeef8484480eef7b058ecb024
Adam Misiak <adammisiak3@gmail.com>
2021-04-13 13:00:56 +0200

    Added login and register tests

--- backend/blogs_manager/users/tests/test_users.py
+++ backend/blogs_manager/users/tests/test_users.py
@@ -1,51 +1,86 @@
-import pytest
+import json
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
 
-from django.test import Client, RequestFactory
-from django.urls import reverse
-from django.contrib.auth.models import User
-
-@pytest.fixture
-def create_user():
-    client = Client()
-    user = User.objects.create_user(username='admin',
-                                 email='admin@admin.com',
-                                 password='admin')
-    return user
+from knox.models import AuthToken
+from users.models import User
+from users.views import UserViewSet, LoginView, RegisterView
 
 
-class TestUsersViews:
-    @pytest.mark.django_db
-    def test_account_view_status_code(self, create_user):
-        client = Client()
-        login = client.login(username="admin", password="admin")
-        url = reverse("account")
-        response = client.get(url)
-        assert response.status_code == 200
+class TestUsers(TestCase):
 
-    @pytest.mark.django_db
-    def test_register_view_status_code(self):
-        client = Client()
-        url = reverse("register")
-        response = client.post(url, {'first_name':'first_name','last_name':'last_name','username':'username','email':'email@email.com','password':'password', 'password2':'password'})
-        assert response.status_code == 302
-        assert response.url == "/"
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user_one = User.objects.create(username="test", email="test@test.com", password="password123", is_active=True)
 
-    @pytest.mark.django_db
-    def test_login_view_status_code(self, create_user):
-        client = Client()
-        url = reverse("login")
-        response = client.post(url, {'username':'admin','password':'admin'})
-        assert response.status_code == 302
-        assert response.url == "/"
+        self.valid_payload_login = {
+            "username": "test2",
+            "password": "password123",
+        }
 
-    @pytest.mark.django_db
-    def test_logout_view_status_code(self, create_user):
-        client = Client()
-        login = client.login(username="admin", password="admin")
-        url = reverse("logout")
-        response = client.get(url)
-        assert response.status_code == 302
-        assert response.url == "/"
+        self.valid_payload_register = {
+            "username": "test2",
+            "email": "test2@test.com",
+            "password": "password123",
+            "password2": "password123",
+        }
+                
+    def test_get_users(self):
+        request =  self.factory.get('/api/users')
+        response = UserViewSet.as_view({'get': 'list'})(request)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
+        assert response.data['results'][0]['id'] == 1
+        assert response.data['results'][0]['username'] == "test"
+        assert response.data['results'][0]['email'] == "test@test.com"
 
 
+    def test_get_user(self):
+        request =  self.factory.get('/api/users/{}'.format(self.user_one.id))
+        response = UserViewSet.as_view({'get': 'retrieve'})(request, pk=self.user_one.id)
 
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == 1
+        assert response.data['username'] == "test"
+        assert response.data['email'] == "test@test.com"
+        assert not response.data['is_superuser']
+
+
+    def test_post_register(self):
+        request =  self.factory.post(
+            '/api/register',
+            content_type='application/json',
+            data=json.dumps(self.valid_payload_register)
+        )
+        response = RegisterView.as_view()(request)
+
+        assert User.objects.all().count() == 2
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['user']['id'] == 2
+        assert response.data['user']['username'] == "test2"
+        assert response.data['user']['email'] == "test2@test.com"
+        assert AuthToken.objects.all().filter(user__username="test2").exists()
+
+    def test_post_login(self):
+        request_login =  self.factory.post(
+            '/api/register',
+            content_type='application/json',
+            data=json.dumps(self.valid_payload_register)
+        )
+        response_login = RegisterView.as_view()(request_login)
+
+        request_register =  self.factory.post(
+            '/api/login',
+            content_type='application/json',
+            data=json.dumps(self.valid_payload_login)
+        )
+        response_register = LoginView.as_view()(request_register)
+
+        assert User.objects.all().count() == 2
+        assert response_register.status_code == status.HTTP_200_OK
+        assert response_register.data['user']['id'] == 2
+        assert response_register.data['user']['username'] == "test2"
+        assert response_register.data['user']['email'] == "test2@test.com"
+        assert AuthToken.objects.all().filter(user__username="test2").exists()
