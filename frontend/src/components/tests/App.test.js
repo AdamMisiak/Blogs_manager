5bc1fcbb12a191d90ccc67b4e259a324b4e4451d
Adam Misiak <adammisiak3@gmail.com>
2021-04-16 13:08:26 +0200

    Added frontend app tests

--- frontend/src/components/tests/App.test.js
+++ frontend/src/components/tests/App.test.js
@@ -1,19 +1,21 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-import React from 'react';
 import { shallow } from 'enzyme';
+import React from 'react';
 import App from '../../App';
 import Header from '../layout/Header';
+import Alerts from '../layout/Alerts'
+
+
 
 it('Shows a header component', () => {
-    const wrapped = shallow(<App />);
-
+    const wrapped = shallow(
+        <App />
+    );
     expect(wrapped.find(Header).length).toEqual(1)
+});
 
+it('Shows a alerts component', () => {
+    const wrapped = shallow(
+        <App />
+    );
+    expect(wrapped.find(Alerts).length).toEqual(1)
 });
