8d8acbdc3a8c70fdbb37d7df53f37f93b2d2e9de
Adam Misiak <adammisiak3@gmail.com>
2021-04-16 14:39:54 +0200

    Added App component tests

--- frontend/src/components/tests/App.test.js
+++ frontend/src/components/tests/App.test.js
@@ -1,21 +1,60 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import { Route } from "react-router-dom";
 import App from '../../App';
+import PrivateRoute from "../common/PrivateRoute";
+import Alerts from '../layout/Alerts';
+import Footer from '../layout/Footer';
 import Header from '../layout/Header';
-import Alerts from '../layout/Alerts'
+import AboutPage from "../screens/AboutPage";
+import BlogDetailsPage from '../screens/BlogDetailsPage';
+import IndexPage from '../screens/IndexPage';
+import LoginPage from '../screens/LoginPage';
+import RegisterPage from '../screens/RegisterPage';
 
+let wrapped;
 
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
 
 it('Shows a header component', () => {
-    const wrapped = shallow(
-        <App />
-    );
     expect(wrapped.find(Header).length).toEqual(1)
 });
 
 it('Shows a alerts component', () => {
-    const wrapped = shallow(
-        <App />
-    );
     expect(wrapped.find(Alerts).length).toEqual(1)
 });
+
+it('Shows a route component', () => {
+    expect(wrapped.find(Route).length).toEqual(5)
+});
+
+it('Shows a private route component', () => {
+    expect(wrapped.find(PrivateRoute).length).toEqual(3)
+});
+
+it('Shows a index page component', () => {
+    expect(wrapped.find(IndexPage).length).toEqual(1)
+});
+
+it('Shows a about page component', () => {
+    expect(wrapped.find(AboutPage).length).toEqual(1)
+});
+
+it('Shows a blog details page component', () => {
+    expect(wrapped.find(BlogDetailsPage).length).toEqual(1)
+});
+
+it('Shows a login page component', () => {
+    expect(wrapped.find(LoginPage).length).toEqual(1)
+});
+
+it('Shows a register page component', () => {
+    expect(wrapped.find(RegisterPage).length).toEqual(1)
+});
+
+it('Shows a footer component', () => {
+    expect(wrapped.find(Footer).length).toEqual(1)
+});
+
