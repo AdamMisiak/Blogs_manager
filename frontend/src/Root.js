e6c64bcbc34f1a4be0731e75f33ef7e7ad84abe3
Adam Misiak <adammisiak3@gmail.com>
2021-04-19 12:15:05 +0200

    Changed root component

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -1,12 +1,11 @@
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import store from './store';
-import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers'; 
+import rootReducer from './reducers';
 
 const alertOptions = {
     timeout: 3000,
@@ -17,14 +16,15 @@ const middleware = [thunk];
 
 export default function Root ({children, initialState = {}}) {
     // CREATED FOR TEST PURPOSE, REPLACE REDUX STORE WITH TESTING INITIAL STATE
+    const store = createStore(
+        rootReducer, 
+        initialState, 
+        composeWithDevTools(applyMiddleware(...middleware))
+    )
 
     return (
         <BrowserRouter>
-            <Provider store={
-                createStore(rootReducer, 
-                            initialState, 
-                            composeWithDevTools(applyMiddleware(...middleware)))
-            }>
+            <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
                     {children}
                 </AlertProvider>
