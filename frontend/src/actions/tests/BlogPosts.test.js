02261bcc543ff1f8727b7333117b238a5047e29c
Adam Misiak <adammisiak3@gmail.com>
2021-04-21 17:53:16 +0200

    Added blog posts action test

--- frontend/src/actions/tests/BlogPosts.test.js
+++ frontend/src/actions/tests/BlogPosts.test.js
@@ -25,9 +25,27 @@ describe('getBlogPostsSuccess', () => {
     })
 
     it('has the correct payload', () => {
-        const action = getBlogPostsSuccess('blogs details');
-
-        expect(action.payload).toEqual('blogs details')
+        const action = getBlogPostsSuccess([{
+            "id": 589,
+            "blog": {
+                "id": 8,
+                "name": "Just Geek It",
+                "url": "https://geek.justjoin.it/",
+                "author": "Just Join It",
+                "genre": "IT",
+                "genre2": "",
+                "language": "Polish"
+            },
+            "name": "Kalendarz konferencji IT 2021 – gdzie warto wybrać się online w tym roku?",
+            "url": "https://geek.justjoin.it/kalendarz-konferencje-it-2021",
+            "date": "2021-04-16T00:00:00Z"
+        }]);
+        expect(action.payload[0].id).toEqual(589)
+        expect(action.payload[0].blog.name).toEqual("Just Geek It")
+        expect(action.payload[0].blog.genre).toEqual("IT")
+        expect(action.payload[0].name).toEqual("Kalendarz konferencji IT 2021 – gdzie warto wybrać się online w tym roku?")
+        expect(action.payload[0].date).toEqual("2021-04-16T00:00:00Z")
+        expect(action.payload.length).toEqual(1)
     })
 })
 
