17083f325050768c6ca7f8a8c5b0b4d6e4517708
Adam Misiak <adammisiak3@gmail.com>
2021-05-12 20:05:07 +0200

    Changed structure of files

--- blogs_manager/settings/settings_prod.py
+++ blogs_manager/settings/settings_prod.py
@@ -8,7 +8,7 @@ DEBUG = True
 SECRET_KEY = 'k5+0g$25=qdggcqcc67aeftf1rqt=n-6^6r-u*(()@u(4%)u4g'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['https://blogs-manager.herokuapp.com/'] 
+ALLOWED_HOSTS = ['blogs-manager.herokuapp.com'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
