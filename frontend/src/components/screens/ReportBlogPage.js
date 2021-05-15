9c00d676da367859978a6b086de12b1c495736c2
Adam Misiak <adammisiak3@gmail.com>
2021-05-15 19:40:07 +0200

    Changed report blog page

--- frontend/src/components/screens/ReportBlogPage.js
+++ frontend/src/components/screens/ReportBlogPage.js
@@ -1,69 +1,63 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Redirect } from "react-router-dom";
-import { login } from '../../actions/Auth';
-import "../../styles/Login.css";
+import "../../styles/ReportBlog.css";
 
 
 function ReportBlogPage() {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [url, setUrl] = useState("")
 
     const auth = useSelector(state => state.auth);
     const dispatch = useDispatch();
 
     const onSubmit = (event) => {
         event.preventDefault();
-        dispatch(login(username, password));
-        setUsername("")
-        setPassword("")
+        // dispatch(login(username, password));
+        setName("")
+        setUrl("")
     };
 
-    const inputUsernameHandler = e => setUsername(e.target.value);
-    const inputPasswordHandler = e => setPassword(e.target.value);
-
-    if (auth.isAuthenticated) return <Redirect to="/" />
+    const inputNameHandler = e => setName(e.target.value);
+    const inputUrlHandler = e => setUrl(e.target.value);
 
     return (
-      <div className="login">
+      <div className="report-blog">
         <div className="container">
           <div className="row">
             <div className="col-md-6 mx-auto">
-              <div className="card-login">
+              <div className="card-report-blog">
                 <div className="card-header bg-primary text-white">
                     <h5>
-                    <i className="fas fa-user-plus"></i> Login</h5>
+                    <i className="fas fa-user-plus"></i> Report Blog</h5>
                 </div>
                 <div className="card-body">
+                <p>Send us you favourite blogs to be able to follow them!</p>
                 <form onSubmit={onSubmit}>
                     <div className="form-group">
-                      <label>Username</label>
+                      <label>Name</label>
                       <input
                         type="text"
                         className="form-control"
-                        name="username"
-                        onChange={inputUsernameHandler}
-                        value={username}
+                        name="name"
+                        onChange={inputNameHandler}
+                        value={name}
                       />
                     </div>
                     <div className="form-group">
-                      <label>Password</label>
+                      <label>URL</label>
                       <input
-                        type="password"
+                        type="text"
                         className="form-control"
-                        name="password"
-                        onChange={inputPasswordHandler}
-                        value={password}
+                        name="url"
+                        onChange={inputUrlHandler}
+                        value={url}
                       />
                     </div>
                     <div className="form-group">
                       <button type="submit" className="btn btn-primary">
-                        Login
+                        Submit
                       </button>
                     </div>
-                    <p>
-                      Don't have an account? <Link to="/register">Register</Link>
-                    </p>
                 </form>
                 </div>
               </div>
