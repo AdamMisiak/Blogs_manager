a1687f7e4b8b2fa9d7f53e4abebc0485ab4f4b87
Adam Misiak <adammisiak3@gmail.com>
2021-05-15 16:34:40 +0200

    Changed redis settings

--- blogs_manager/celery.py
+++ blogs_manager/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogs_manager.settings.settings_prod')
 # app = Celery('blogs_manager')
-app = Celery('blogs_manager', broker='redis://redis:6379/0')
+app = Celery('blogs_manager', broker='redis://:pf2b7503729730fb2075fd836cb91ee9ed408996caecb8198b71bcc37decee05d@ec2-99-81-57-235.eu-west-1.compute.amazonaws.com:21550')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
