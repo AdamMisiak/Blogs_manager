eefbd38fb08fd3d6dd5661e8a2643e6afbeabb79
Adam Misiak <adammisiak3@gmail.com>
2021-06-05 13:35:58 +0200

    Edited requirements

--- backend/blogs_manager/blogs_manager/settings/settings.py
+++ backend/blogs_manager/blogs_manager/settings/settings.py
@@ -91,17 +91,6 @@ WSGI_APPLICATION = "blogs_manager.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'blogs_manager',
-#         'USER': 'adam',
-#         'PASSWORD': 'adam',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
