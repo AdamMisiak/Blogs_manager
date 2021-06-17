67fbd622339bb5c12399ced43ca1ecf7495f6c3e
Adam Misiak <adammisiak3@gmail.com>
2021-06-17 10:59:04 +0200

    Added weekly email function

--- backend/blogs_manager/users/functions.py
+++ backend/blogs_manager/users/functions.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from blogs.functions import *
 from blogs.models import Blog, BlogPost
@@ -99,3 +99,48 @@ def send_daily_newsletter():
                 fail_silently = False,
             )
         logger.error("Daily blog posts report sent to user {}".format(user))
+
+# Greenwich timezone
+@periodic_task(run_every=(crontab(minute=0, hour=2, day_of_week='mon')), name="send_weekly_newsletter", ignore_result=True)
+def send_weekly_newsletter():
+    today = date.today()
+    start_date = today - timedelta(days=7)
+    users = User.objects.filter(
+        is_active=True, 
+        email_setting__email_frequency="weekly")
+    blog_posts = BlogPost.objects.filter(
+        date__range=[start_date, today]
+    ).order_by('-id')
+    for user in users:
+        content = 'Weekly blog posts report from {}-{}-{} to {}-{}-{}:\n'.format(
+            start_date.day, 
+            start_date.month, 
+            start_date.day, 
+            today.day, 
+            today.month, 
+            today.year
+        )
+        content_exists = False
+        for blog_post in blog_posts:
+            user_is_subscribing = BlogSubscriber.objects.filter(blog__blog_post=blog_post, user=user)
+            if user_is_subscribing:
+                content += "--------------------------------------- \n"
+                content += 'BLOG POST: {}\nBLOG: {}\nDATE: {}\nLINK: {}'.format(
+                    blog_post.name.strip(), 
+                    blog_post.blog, 
+                    blog_post.date.strftime('%d-%m-%Y'), 
+                    blog_post.url
+                )
+                content += " \n"
+                content_exists = True
+        if content_exists:
+            send_mail(
+                subject = 'Daily blog posts report {}-{}-{}'.format(today.day-1, today.month, today.year),
+                message = content,
+                from_email = 'adammi.adam@gmail.com',
+                recipient_list = ['adammisiak3@gmail.com',],
+                fail_silently = False,
+            )
+        logger.error("Weekly blog posts report sent to user {}".format(user))
+
+        
--- backend/blogs_manager/users/management/commands/send_mail.py
+++ backend/blogs_manager/users/management/commands/send_mail.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.core.mail import send_mail
 
-from datetime import date
+from datetime import date, timedelta
 
 from users.models import User, BlogSubscriber
 from blogs.models import BlogPost
@@ -10,30 +10,43 @@ from blogs.models import BlogPost
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         today = date.today()
+        start_date = today - timedelta(days=7)
         users = User.objects.filter(
             is_active=True, 
-            email_setting__email_frequency="daily")
+            email_setting__email_frequency="weekly")
         blog_posts = BlogPost.objects.filter(
-            date__day=today.day - 1,
-            date__month=today.month,
-            date__year=today.year
+            date__range=[start_date, today]
         ).order_by('-id')
         for user in users:
-            content = 'Daily blog posts report {}-{}-{}:\n'.format(today.day-1, today.month, today.year)
+            content = 'Weekly blog posts report from {}-{}-{} to {}-{}-{}:\n'.format(
+                start_date.day, 
+                start_date.month, 
+                start_date.day, 
+                today.day, 
+                today.month, 
+                today.year
+            )
             content_exists = False
             for blog_post in blog_posts:
                 user_is_subscribing = BlogSubscriber.objects.filter(blog__blog_post=blog_post, user=user)
                 if user_is_subscribing:
                     content += "--------------------------------------- \n"
-                    content += 'BLOG POST: {}\nBLOG: {}\nLINK: {}'.format(blog_post.name.strip(), blog_post.blog, blog_post.url)
+                    content += 'BLOG POST: {}\nBLOG: {}\nDATE: {}\nLINK: {}'.format(
+                        blog_post.name.strip(), 
+                        blog_post.blog, 
+                        blog_post.date.strftime('%d-%m-%Y'), 
+                        blog_post.url
+                    )
                     content += " \n"
                     content_exists = True
             if content_exists:
                 send_mail(
-                    subject = 'Blog posts report {}-{}-{}'.format(today.day-1, today.month, today.year),
+                    subject = 'Daily blog posts report {}-{}-{}'.format(today.day-1, today.month, today.year),
                     message = content,
                     from_email = 'adammi.adam@gmail.com',
                     recipient_list = ['adammisiak3@gmail.com',],
                     fail_silently = False,
                 )
-            # logger.error("Email to user {} from {} blog has been send".format(user, blog_post.blog.name))
+            # logger.error("Weekly blog posts report sent to user {}".format(user))
+
+        
