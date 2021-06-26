43d3d7cc84ee8000b6efff6c75ff9bd518676ea3
Adam <adammisiak3@gmail.com>
2021-06-26 13:01:12 +0200

    Added another blog functions

--- blogs/functions.py
+++ blogs/functions.py
@@ -46,7 +46,6 @@ def get_info_from_trading_for_a_living():
         page = requests.get("https://www.tradingforaliving.pl/", headers=headers)
         soup = BeautifulSoup(page.content, "html.parser")
         blog_post_list = []
-        
         blog_post_title = str(
             soup.find("div", class_="c-post-card__title c-post-card__title--featured").text
         )[1:]
@@ -674,7 +673,92 @@ def get_info_from_make_life_easier():
 
         return blog_post_list
     except:
-        logger.error("Something went wrong in scraping function: get_info_from_dividends_and_income")
+        logger.error("Something went wrong in scraping function: get_info_from_make_life_easier")
+
+
+def get_info_from_rewolucja_energetyczna():
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+        }
+        page = requests.get("https://rewolucjaenergetyczna.wordpress.com/", headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        blog_post_list = []
+
+        blog_post_title = soup.find("div", class_="column column-content posts").find_next('div').find_next('h2').find_next('a').text
+        blog_post_link = soup.find("div", class_="column column-content posts").find_next('div').find_next('h2').find_next('a')['href']
+        
+        blog_post_day = soup.find("div", class_="column column-content posts").find_next('div').find_next('p').find_next('a').text[:2]
+        blog_post_month = str(month_string_to_date(soup.find("div", class_="column column-content posts").find_next('div').find_next('p').find_next('a').text[2:6].strip()))
+        if blog_post_month == 0:
+            blog_post_month = str(month_string_to_date(soup.find("div", class_="column column-content posts").find_next('div').find_next('p').find_next('a').text[1:5].strip()))
+        blog_post_year = str(soup.find("div", class_="column column-content posts").find_next('div').find_next('p').find_next('a').text[-10:-5].strip())
+        blog_post_date_string = blog_post_day + "." + blog_post_month + "." + blog_post_year
+        blog_post_date = datetime.datetime.strptime(blog_post_date_string, "%d.%m.%Y").date()
+
+        blog_post_list.append(blog_post_title)
+        blog_post_list.append(blog_post_link)
+        blog_post_list.append(blog_post_date)
+
+        return blog_post_list
+    except:
+        logger.error("Something went wrong in scraping function: get_info_from_rewolucja_energetyczna")
+
+
+def get_info_from_breadcrumbs_collector():
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+        }
+        page = requests.get("https://breadcrumbscollector.tech/", headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        blog_post_list = []
+
+        blog_post_title = soup.find("h2", class_="entry-title").find_next('a').text
+        blog_post_link = soup.find("h2", class_="entry-title").find_next('a')['href']
+        
+        blog_post_comma_index = int(str(soup.find("span", class_="posted-on").find_next('a').find_next('time').text).find(','))
+        blog_post_day = soup.find("span", class_="posted-on").find_next('a').find_next('time').text[blog_post_comma_index-2:blog_post_comma_index].strip()
+        blog_post_month = str(month_string_to_date(soup.find("span", class_="posted-on").find_next('a').find_next('time').text[:3]))
+        blog_post_year = str(soup.find("span", class_="posted-on").find_next('a').find_next('time').text[-4:].strip())
+        blog_post_date_string = blog_post_day + "." + blog_post_month + "." + blog_post_year
+        blog_post_date = datetime.datetime.strptime(blog_post_date_string, "%d.%m.%Y").date()
+
+        blog_post_list.append(blog_post_title)
+        blog_post_list.append(blog_post_link)
+        blog_post_list.append(blog_post_date)
+
+        return blog_post_list
+    except:
+        logger.error("Something went wrong in scraping function: get_info_from_breadcrumbs_collector")
+
+
+def get_info_from_dividend_stocks():
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+        }
+        page = requests.get("https://dividendstocks.cash/blog/", headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        blog_post_list = []
+
+        blog_post_title = soup.find("h2", class_="entry-title").find_next('a').text
+        blog_post_link = soup.find("h2", class_="entry-title").find_next('a')['href']
+
+        blog_post_comma_index = int(str(soup.find("span", class_="post-date updated").text).find(','))
+        blog_post_day = soup.find("span", class_="post-date updated").text[blog_post_comma_index-2:blog_post_comma_index].strip()
+        blog_post_month = str(month_string_to_date(soup.find("span", class_="post-date updated").text[:3]))
+        blog_post_year = str(soup.find("span", class_="post-date updated").text[-4:].strip())
+        blog_post_date_string = blog_post_day + "." + blog_post_month + "." + blog_post_year
+        blog_post_date = datetime.datetime.strptime(blog_post_date_string, "%d.%m.%Y").date()
+
+        blog_post_list.append(blog_post_title)
+        blog_post_list.append(blog_post_link)
+        blog_post_list.append(blog_post_date)
+
+        return blog_post_list
+    except:
+        logger.error("Something went wrong in scraping function: get_info_from_dividend_stocks")
 
 
 # result = get_info_from_inwestomat()
@@ -724,4 +808,10 @@ def get_info_from_make_life_easier():
 # result = get_info_from_dividends_and_income()
 # print(result)
 # result = get_info_from_make_life_easier()
+# print(result)
+# result = get_info_from_rewolucja_energetyczna()
+# print(result)
+# result = get_info_from_breadcrumbs_collector()
+# print(result)
+# result = get_info_from_dividend_stocks()
 # print(result)
